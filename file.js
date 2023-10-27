a272acac0e952ea6e7c5738072a513e144a85449
diego <diegozaratean@gmail.com>
2023-10-27 13:35:16 +0000

    finish landin page react class 1

---
+++ file.js
@@ -0,0 +1,10 @@
+//importar funcion sum
+import { sum, resta } from './file2.js'
+import unica from './file2.js'
+
+//ejecutar y mostrar en cosonla el resutlado de la funcion sum
+console.log(sum(2,3))
+
+console.log(resta(5,2))
+
+console.log(unica(5,2))
---
+++ file2.js
@@ -0,0 +1,21 @@
+// crear la funcion sum
+let sum = (a,b) => {
+    return a+b;
+};
+// console.log(sum(5,6))
+
+let resta = (a,b) => {
+    return a-b;
+};
+
+let unica = (a,b) => {
+    return a*b;
+};
+
+// exportar la funcion sum
+export {
+    sum, resta
+};
+
+
+export default unica;
